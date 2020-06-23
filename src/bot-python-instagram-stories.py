f3b2f932ecceb1c0f93f76a55dd6dbf32b15fcd7
Paulo <al.pdias24@gmail.com>
2020-06-22 21:03:32 -0300

    update all src

--- src/bot-python-instagram-stories.py
+++ src/bot-python-instagram-stories.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 '''
 Created in 12/2019
 @Author: Paulo https://github.com/alpdias
@@ -11,62 +12,84 @@ from time import sleep
 import random
 import os
 
+# input for config bot
+os.system('cls') # for linux user 'clear' and for windows use 'cls'
+print('')
+print('\033[0;32mCONFIGURATION\033[m')
+print('')
+way = str(input('Way: ')) # way to geckodriver
+delay = int(input('Delay: ')) # loading delay time
+
 # input info for bot 
-os.system('cls')
+os.system('cls') # for linux user 'clear' and for windows use 'cls'
 print('')
 print('\033[0;32mLOGIN INFORMATION\033[m')
 print('')
 username = str(input('User: ')) # your user
 password = str(input('Password: ')) # your password
-os.system('cls')
+
+os.system('cls') # for linux user 'clear' and for windows use 'cls'
 print('')
 print('Loading...')
 print('')
 
 # load browser drive in to var and open
 try:
-    driver = webdriver.Firefox(executable_path=r'add here way to geckodriver.exe') # geckodriver path https://github.com/mozilla/geckodriver/releases/tag/v0.26.0
+    driver = webdriver.Firefox(executable_path=f'{way}') # geckodriver path https://github.com/mozilla/geckodriver/releases/tag/v0.26.0
 except:
     print('\033[0;31mDRIVER ERROR!\033[m Check installed drive or path.')
 
 # function to access the login page and log in
 def botlogin (user, pwd):
+
     username = user # your user
     password = pwd # your password
+
     driver.get('https://www.instagram.com/') # instagram url
+    sleep(delay)
 
     '''
     this page / button was removed by Instagram
-
-    sleep(1)
     driver.find_element_by_xpath('//a[@href="/accounts/login/?source=auth_switcher"]').click() # click on the 'connect' button 
     '''
     
-    sleep(2)
     userelement = driver.find_element_by_xpath('//input[@name="username"]') # 'username' input element
     userelement.clear()
     userelement.send_keys(username) # user insertion in 'user' element
+
     pwdelement = driver.find_element_by_xpath('//input[@name="password"]') # 'password 'input element
     pwdelement.clear()
     pwdelement.send_keys(password) # password insertion in 'password' element
+
     pwdelement.send_keys(Keys.RETURN) # log in to page
     sleep(4)
 
 
 # function to view the stories
 def stories():
-    sleep(2)
+
     try:
         driver.find_element_by_xpath('//button[contains(text(), "Agora não")]').click() # press the notification button that appears most of the time, denying the option
-        sleep(2)
+        sleep(delay)
     except:
         pass
-    driver.find_element_by_xpath('//div[contains(text(), "Ver tudo")]').click() # click on the tab where the stories are
-    sleep(2)
+
+    try:
+        driver.find_element_by_xpath('//button[contains(text(), "Agora não")]').click() # press the notification button that appears most of the time, denying the option
+        sleep(delay)
+    except:
+        pass
+
+    driver.find_element_by_class_name('_6q-tv').click() # click on the tab where the stories are
+    sleep(delay)
+
     loadstories = '' # variable to terminate the loop without errors
+
     while loadstories != 0:
+
+        sleep(delay)
+
         try:
-            sleep(2)
             driver.find_element_by_class_name('coreSpriteRightChevron').click() # next storie button
         except KeyboardInterrupt:
             print('\033[0;33mProgram terminated by the user!\033[m')
@@ -76,29 +99,22 @@ def stories():
             loadstories = 0
 
 
-# execution of functions
+# running function for login
 try:
     botlogin(username, password)
 except KeyboardInterrupt:
     print('\033[0;33mProgram terminated by the user!\033[m')
 except:
     print('\033[0;31mUNEXPECTED ERROR ON LOGIN\033[m, please try again and verify your connection!')
+
+# running function to see the stories
 try:
     stories()
 except KeyboardInterrupt:
     print('\033[0;33mProgram terminated by the user!\033[m')
 except:
     print('\033[0;31mUNEXPECTED ERROR ON STORIES\033[m, please try again and verify your connection!')
+
 print('')
 print('Finish!')
 print('')
-
-'''
-# references html
-# //a[@href="/accounts/login/?source=auth_switcher"] --> 'connect' page button element
-# //input[@name="username"] --> 'username' input element
-# //input[@name="password"] --> 'password 'input element
-# //button[contains(text(), "Agora não")] --> the notification button
-# //div[contains(text(), "Ver tudo")] --> the tab where the stories are
-# @class="coreSpriteRightChevron" --> next storie button
-'''
