35aa091fbc6f16b7481c2efdf46e9e629279a277
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-07-28 14:29:08 -0300

    Update bot-instagram.py

--- src/bot-instagram.py
+++ src/bot-instagram.py
@@ -4,3 +4,17 @@
 Created in 07/2020
 @Author: Paulo https://github.com/alpdias
 '''
+
+# imported libraries
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import random
+import os
+from pyfiglet import Figlet
+
+# print text in ascii art
+f = Figlet(font='slant')
+instagramName = f.renderText('Instagram bot')
+
+print(instagramName)
