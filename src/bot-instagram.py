f9be53367c837738c5fe65c6acafdb8a7493b7b9
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-07-29 13:59:13 -0300

    Update bot-instagram.py

--- src/bot-instagram.py
+++ src/bot-instagram.py
@@ -6,12 +6,34 @@ Created in 07/2020
 '''
 
 # imported libraries
+import os
 import art
 import botLike
 import botComment
 import botStories
 
 art.artName()
-#botLike.functionLike()
-#botComment.functionComment()
-#botStories.functionStories()
+
+menu = ['Like', 'Comment and Like', 'View Stories']
+
+for indice, lista in enumerate(menu): # loop to generate an index in the list of options
+    print(f'\033[0;34m[{indice}]\033[m {lista}') # print the list of options
+
+print('')
+selected = int(input('Select a function for the bot: ')) # receive the function that will be started
+print('')
+
+if selected == 0:
+    botLike.functionLike() # bot to like
+    
+elif selected == 1:
+    botComment.functionComment() # bot to like and comment
+    
+elif selected == 2:
+    botStories.functionStories() # bot to see stories
+    
+else:
+  print('Option invalid, please try again!')
+
+os.system('TASKKILL /F /IM cmd.exe') # close the CMD.exe
+os.system('TASKKILL /F /IM powershell.exe') # close the PowerShell.exe
