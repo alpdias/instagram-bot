1f2a06bd5496e52866d442993f38b687464e5503
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-11-11 16:04:14 -0300

    Update botInstagram.py

--- src/botInstagram.py
+++ src/botInstagram.py
@@ -22,21 +22,28 @@ while True:
     menu = ['Like', 'Comment and Like', 'View Stories']
 
     for indice, lista in enumerate(menu): # loop to generate an index in the list of options
+        
         print(f'\033[0;34m[{indice}]\033[m {lista}') # print the list of options
 
     print('')
     print('\033[0;33m(to finish press Ctrl + C)\033[m')
+    
     selected = int(input('Select a function for the bot: ')) # receive the function that will be started
+    
     print('')
 
     if selected == 0:
+        
         botLike.functionLike(mySystem) # bot to like
 
     elif selected == 1:
+        
         botComment.functionComment(mySystem) # bot to like and comment
 
     elif selected == 2:
+        
         botStories.functionStories(mySystem) # bot to see stories
 
     else:
+        
       print('Option invalid, please try again!')
